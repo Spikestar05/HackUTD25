0506a06012d524881c926d795c19d4057a2e6877
Aagam Shah <shahaagam04@gmail.com>
2025-11-09 06:08:56 -0600

    feat: Installed tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  vite:{
+    plugins: [
+      tailwindcss()
+    ]
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
+    '@nuxt/ui',
+    '@tresjs/nuxt'
+  ],
+  tres: {
+    devtools: true
+  }
 })
