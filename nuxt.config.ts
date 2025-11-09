37f0c5aadb04d69d64d3b55909e7ea3bf4d69073
Aagam Shah <shahaagam04@gmail.com>
2025-11-09 10:02:38 -0600

    fix: Correct CSS array syntax in nuxt.config.ts and ensure proper module formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
   vite:{
     plugins: [
       tailwindcss()
@@ -15,7 +16,7 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     '@nuxt/test-utils',
     '@nuxt/ui',
-    '@tresjs/nuxt'
+    '@tresjs/nuxt',
   ],
   tres: {
     devtools: true
